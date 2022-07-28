458d5f5f977321f66395be460c45d44da1659e76
Senior Guru <polarbear91781014@gmail.com>
2022-07-28 16:09:52 -0600

    migrate

--- frontend/src/static/constants.js
+++ frontend/src/static/constants.js
@@ -1,5 +1,5 @@
 export const DefaultLocale = 'EN';
 
-export const PRIVATE_MARKETSMITH_API = "http://10.10.10.36:8000/api/" ;
-// export const PRIVATE_MARKETSMITH_API = "http://127.0.0.1:5000/api/" ;
+// export const PRIVATE_MARKETSMITH_API = "http://10.10.10.36:8000/api/" ;
+export const PRIVATE_MARKETSMITH_API = "http://127.0.0.1:5000/api/" ;
 export const PUBLIC_MARKETSMITH_API = "http://127.0.0.1:5000/marketsmith/files/";
