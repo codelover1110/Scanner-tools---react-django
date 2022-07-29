68512b38f4142e8bb96d720de7448fd7360a532e
codelover1110 <dev1110upwork@gmail.com>
2022-07-29 15:21:34 -0500

    deploying 0729

--- frontend/src/static/constants.js
+++ frontend/src/static/constants.js
@@ -1,7 +1,2 @@
 export const DefaultLocale = 'EN';
-
-// export const PRIVATE_MARKETSMITH_API = "http://10.10.10.191:8000/api/" ;
-// export const PRIVATE_MARKETSMITH_API = "http://127.0.0.1:5000/api/" ;
-// export const PUBLIC_MARKETSMITH_API = "http://127.0.0.1:5000/marketsmith/files/";
-export const PRIVATE_MARKETSMITH_API = "https://164.92.114.205:8080/api/" ;
-export const PUBLIC_MARKETSMITH_API = "https://164.92.114.205:8080/marketsmith/files/";
+export const PRIVATE_MARKETSMITH_API = "http://164.92.114.205:8080/api/" ;
