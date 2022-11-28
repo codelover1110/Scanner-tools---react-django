11587a9fc09ddd7f3e4ebab7f1b826abf3a540c1
Script Kiddie <jotdevelopers@gmail.com>
2022-11-28 20:03:39 +0500

    Data Grid Implemented

--- frontend/src/static/constants.js
+++ frontend/src/static/constants.js
@@ -1,2 +1,3 @@
 export const DefaultLocale = 'EN';
 export const PRIVATE_MARKETSMITH_API = "http://164.92.114.205:8080/api/" ;
+//export const PRIVATE_MARKETSMITH_API = "http://127.0.0.1:8000/api/" ;
